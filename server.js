6e35a624f60fdcbaa1ee44369a25c88300e12e21
Nattawod Satee <nattawodsatee@gmail.com>
2019-06-14 16:13:18 -0700

    Prop for heroku login

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up the production asset
+    // like main.js or main.css!
+    app.use(express.static('client/build'));
+    // express will serve up the index.html file
+    // if it doesn't reconize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
