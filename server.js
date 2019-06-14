61d6580bf17cf8543c43c2a6a5be10e812a0ea16
Nattawod Satee <nattawodsatee@gmail.com>
2019-06-14 16:41:19 -0700

    Fixing deploy still (add the ping)

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
+var reqTimer = setTimeout(function wakeUp() {
+    request("https://my-qa.herokuapp.com/", function() {
+        console.log("WAKE UP DYNO");
+    });
+    return (reqTimer = setTimeout(wakeUp, 1200000));
+}, 1200000);
+
+if (process.env.NODE_ENV === "production") {
     // Express will serve up the production asset
     // like main.js or main.css!
-    app.use(express.static('client/build'));
+    app.use(express.static("client/build"));
     // express will serve up the index.html file
     // if it doesn't reconize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
